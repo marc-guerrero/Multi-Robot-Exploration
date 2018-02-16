3159fe2b5abbb4cdf8653bafb0084e0c262ad784
Marc Guerrero <cayman.north@gmail.com>
2018-02-15 21:10:20 -0500

    Decouples main function from Multi Robot Exploration algorithm

--- src/main/java/multi/robot/exploration/RobotExploration.java
+++ src/main/java/multi/robot/exploration/RobotExploration.java
@@ -186,26 +186,4 @@ public class RobotExploration
 
         return false;
     }
-
-    public static void main(String[] args)
-    {
-        // TODO: Handle file input.
-        int t = 100;
-        Position initLoc[] = {new Position(0,9), new Position(1,9), new Position(2,9)};
-        Position obstacles[] = {new Position(0,0), new Position(1,0), new Position(2,2),
-                                new Position(3,3), new Position(4,4), new Position(4,4),
-                                new Position(5,5), new Position(6,6), new Position(7,7),
-                                new Position(8,8), new Position(9,9)};
-
-        RobotExploration algo = new RobotExploration(10, 10, 10, 3, initLoc, obstacles);
-
-        // run algorithm for t (time) steps
-        for (int i = 0; i < t; ++i)
-        {
-            if (algo.runInteration())
-            {
-                return;
-            }
-        }
-    }
 }
